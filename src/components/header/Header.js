3571f74c0364469074a6864d48e7614232a13f05
Stephen Coloma <2232847@slu.edu.ph>
2024-09-10 00:04:15 +0800

    add: added the header of the project

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,31 @@
+import styles from '../header/header.module.css'
+
 export default function Header() {
     return (
-      <nav>
-        <div>
-          <img src="../assets/hublot-logo-header.svg" alt="Logo" />
+      <header>
+
+        <nav className={styles.nav}>
+          <div className={styles.imageContainer}>
+            <img id={styles.burger} src="../assets/burger-header.svg" alt="Logo" />
+          </div>
+          <div className={styles.imageContainer} id={styles.logo}>
+            <img src="../assets/hublot-logo-header.svg" alt="Logo" />
+          </div>
+          <div className={styles.imageContainer}>
+            <img src="../assets/bag-header.svg" alt="Logo" />
+          </div>
+          <div className={styles.imageContainer}>
+            <img src="../assets/profile-header.svg" alt="Logo" />
+          </div>
+
+        </nav>
+
+        <div className={styles.search}>
+            <div>
+              <img id={styles.searchIcon} src='../assets/search-header.svg'></img>
+            </div>
+            <h2>What are you looking for?</h2>
         </div>
-      </nav>
+      </header>
     );
   }
