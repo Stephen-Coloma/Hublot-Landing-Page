c62bd412ef436d1a2753893fed2b41bffc8779b0
Stephen Coloma <2232847@slu.edu.ph>
2024-09-10 21:19:36 +0800

    add: added the web design for the header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,22 +5,44 @@ export default function Header() {
       <header>
 
         <nav className={styles.nav}>
-          <div className={styles.imageContainer}>
+
+          <div className={`${styles.imageContainer} ${styles.mobile}`}>
             <img id={styles.burger} src="../assets/burger-header.svg" alt="Logo" />
           </div>
-          <div className={styles.imageContainer} id={styles.logo}>
+
+          <div className={styles.imageContainer} id={styles.logoContainer}>
             <img src="../assets/hublot-logo-header.svg" alt="Logo" />
           </div>
+
+          {/* watches, our world and boutiques */}
+          <div className={styles.web} id={styles.textContainer}>
+            <h2>WATCHES</h2>
+            <h2>OUR WORLD</h2>
+            <h2>BOUTIQUES</h2>
+          </div>
+
+          <div className={`${styles.imageContainer} ${styles.web}`}>
+              <img src='../assets/heart-header.svg'></img>
+          </div>
+          <div className={`${styles.imageContainer} ${styles.web}`}>
+          <img src='../assets/search-header.svg'></img>
+          </div>
+
           <div className={styles.imageContainer}>
             <img src="../assets/bag-header.svg" alt="Logo" />
           </div>
-          <div className={styles.imageContainer}>
+
+          <div className={`${styles.imageContainer} ${styles.mobile}`}>
             <img src="../assets/profile-header.svg" alt="Logo" />
           </div>
 
+          <div className={styles.web} id={styles.hublotista}>
+            <h2>HUBLOTISTA</h2>
+            <img src='../assets/profile-header.svg'></img>
+          </div>
         </nav>
 
-        <div className={styles.search}>
+        <div id={styles.search}>
             <div>
               <img id={styles.searchIcon} src='../assets/search-header.svg'></img>
             </div>
