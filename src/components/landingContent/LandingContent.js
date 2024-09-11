b23e791f09cd159d083bfcfafdb52537ff656625
Stephen Coloma <2232847@slu.edu.ph>
2024-09-11 23:22:53 +0800

    add: finished the first part of the page

--- src/components/landingContent/LandingContent.js
+++ src/components/landingContent/LandingContent.js
@@ -1,10 +1,30 @@
+'use client'
+
+import { useState } from 'react';
 import styles from '../landingContent/landingContent.module.css';
 
 export default function LandingContent(){
+    
+    const [image, setImage] = useState('/assets/main-watch-1.png')
+    const [circle1, setCircle1] = useState(styles.circleV2)
+    const [circle2, setCircle2] = useState(styles.circleV1)
+
+    const handleLeftClick = () => {
+        setImage('/assets/main-watch-1.png')
+        setCircle1(styles.circleV2)
+        setCircle2(styles.circleV1)
+    }
+
+    const handleRightClick = () => {
+        setImage('/assets/main-watch-2.png')
+        setCircle1(styles.circleV1)
+        setCircle2(styles.circleV2)
+    }
+
     return(
         <>
             <div className={styles.mainContainer}>
-                <img src='/assets/main-watch-1.png'></img>
+                <img src={image}></img>
 
                 <div className={styles.container}>
                     <div className={styles.content}>
@@ -16,10 +36,25 @@ export default function LandingContent(){
                                 <img src='/assets/bag-white.svg'></img>
                             </div>
                             <h2 id={styles.separator}>|</h2>
-                            <h2>ONLINE EXCLUSIVE</h2>
+                            <h2 id={styles.exclusive}>ONLINE EXCLUSIVE</h2>
                         </div>
                     </div>
-                </div>                
+                </div>      
+
+                <div className={styles.bellowButtons}>
+                    <div className={styles.arrowContainer}>
+                        <button onClick={handleLeftClick}><img src='/assets/left-arrow.svg'></img></button>
+                    </div>
+
+                    <div className={styles.middleContainer}>
+                        <div className={circle1}></div>
+                        <div className={circle2}></div>
+                    </div>
+
+                    <div className={styles.arrowContainer}>
+                        <button onClick={handleRightClick}><img src='/assets/right-arrow.svg'></img></button>
+                    </div>
+                </div>          
             </div>
             
         </>
