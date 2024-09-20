a14305bf01da35a57f1a21105a4c4754bc44e7ce
Stephen Coloma <2232847@slu.edu.ph>
2024-09-20 22:32:34 +0800

    add: added the template for routing inside the website

---
+++ src/app/boutiques/page.js
@@ -0,0 +1,13 @@
+'use client'
+
+import Footer from "@/components/footer/Footer"
+import Header  from "@/components/header/Header"
+
+export default function Botiques(){
+    return(
+        <>
+            <Header></Header>
+            <Footer></Footer>
+        </>
+    )
+}
