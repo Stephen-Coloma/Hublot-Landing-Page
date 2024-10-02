0afc0784db13392cf2067f5da38e5830199be7b7
Stephen Coloma <2232847@slu.edu.ph>
2024-10-02 22:47:37 +0800

    add: added the first part of the our world page

--- src/app/our-world/page.js
+++ src/app/our-world/page.js
@@ -1,5 +1,7 @@
 'use client'
 
+import styles from '@/app/our-world/our-world.module.css'
+
 import Footer from "@/components/footer/Footer"
 import Header  from "@/components/header/Header"
 
@@ -7,6 +9,22 @@ export default function OurWorld(){
     return(
         <>
             <Header></Header>
+                <div className={styles.topContainer}>
+                    <img src='/assets/brown-man.jpg'></img>
+                    <div className={styles.textContainer}>
+                        <h4>WELCOME TO</h4>
+                        <h2>OUR WORLD</h2>
+                    </div>
+                </div>
+
+                <div className={styles.quoteContainer}>
+                    <p id={styles.quote}>
+                        Exploring every detail of innovation, 
+                        Hublot’s world is made of groundbreaking Craftsmanship, 
+                        Partnerships and News & Events.
+                    </p>
+                </div>
+
             <Footer></Footer>
         </>
     )
